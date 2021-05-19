4a98d37c1c4e4c81cc496555df0f1b1df4795a0c
Lunush <76045303+lunush@users.noreply.github.com>
2021-05-19 12:10:13 +0300

    Add longestRaisingSequence functionality

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,32 @@
 import "reflect-metadata";
 import { ApolloServer } from "apollo-server-express";
 import express from "express";
-import { Resolver, Query, buildSchema } from "type-graphql";
+import { Resolver, Query, buildSchema, Arg, Int } from "type-graphql";
+
+const getLongestRaisingSequence = (nums: number[]) => {
+  if (!nums.length) return [];
+
+  let temp = [nums[0]];
+  let res: number[] = [];
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i - 1] <= nums[i]) temp.push(nums[i]);
+    else {
+      if (temp.length > res.length) res = temp;
+      temp = [nums[i]];
+    }
+  }
+
+  if (temp.length > res.length) res = temp;
+
+  return res;
+};
 
 @Resolver()
 class MyResolver {
-  @Query(() => String)
-  longestRaisingSequence() {
-    return "hi";
+  @Query(() => [Int])
+  longestRaisingSequence(@Arg("nums", () => [Int]) nums: number[]) {
+    return getLongestRaisingSequence(nums);
   }
 }
 
